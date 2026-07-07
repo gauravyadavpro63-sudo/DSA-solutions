b45cd5d4db5052e85f7d305fdd6c97db45312f1c
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-07-07 12:57:36 +0530

    recursion

--- step46.cpp
+++ step46.cpp
@@ -76,29 +76,29 @@ void generate_all_subsequence_of_string(string s){
     string current="";
     solve(current,index,s);
 }
-// void solve(int index,vector<int>&ans,int sum,vector<int>arr,int solving){
-//   if(index==arr.size()){
-//     if(solving==sum){
-//         for(auto it:ans){
-//             cout<<it<<" ";
-//         }
-//         cout<<endl;
-//     }
-//     return;
-//   }
-//   ans.push_back(arr[index]);
-//   solving+=arr[index];
-//   solve(index+1,ans,sum,arr,solving);
-//   ans.pop_back();
-//   solving-=arr[index];
-//   solve(index+1,ans,sum,arr,solving);
-// }
-// void print_all_subsequence_of_sum_2(int sum,vector<int>arr){
-// int index=0;
-// vector<int>ans;
-// int solving=0;
-// solve(index,ans,sum,arr,solving);
-// }
+void solve(int index,vector<int>&ans,int sum,vector<int>arr,int solving){
+  if(index==arr.size()){
+    if(solving==sum){
+        for(auto it:ans){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+    return;
+  }
+  ans.push_back(arr[index]);
+  solving+=arr[index];
+  solve(index+1,ans,sum,arr,solving);
+  ans.pop_back();
+  solving-=arr[index];
+  solve(index+1,ans,sum,arr,solving);
+}
+void print_all_subsequence_of_sum_2(int sum,vector<int>arr){
+int index=0;
+vector<int>ans;
+int solving=0;
+solve(index,ans,sum,arr,solving);
+}
 // bool solve(int index,vector<int>&ans,int solving,vector<int>arr,int sum){
 //     if(index==arr.size()){
 //         if(solving==sum){
@@ -144,31 +144,159 @@ void generate_all_subsequence_of_string(string s){
 //     int solving=0;
 //    return solve(index,solving,arr,sum);
 // }
-void solve(vector<int>arr,int target,vector<int>&current,vector<vector<int>>&ans,int index){
-  if(index==arr.size()){
-    if(target==0){
-        ans.push_back(current);
+// void solve(vector<int>arr,int target,vector<int>&current,vector<vector<int>>&ans,int index){
+//   if(index==arr.size()){
+//     if(target==0){
+//         ans.push_back(current);
+//     }
+//     return ;
+//   }
+//   if(arr[index]<=target){
+//     current.push_back(arr[index]);
+//     solve(arr,target-arr[index],current,ans,index);
+//     current.pop_back();
+//   }
+//   solve(arr,target,current,ans,index+1);
+// }
+// void combination_sum(vector<int>arr,int target){
+// vector<int>current;
+// vector<vector<int>>ans;
+// int index=0;
+// solve(arr,target,current,ans,index);
+// for(auto it :ans){
+//     for(auto x:it){
+//         cout<<x<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+//   brute forcea tc=2^n*logk where k in number of stored answer in k
+// void  solve(vector<int>&candidates,
+//           int target,
+//           set<vector<int>>&ans,
+//           int index,
+//           vector<int>&current,
+//           int solving)
+// {
+// if(index==candidates.size()){
+//     if(solving==target){
+//         ans.insert(current);
+//     }
+//     return;
+// }
+// current.push_back(candidates[index]);
+// solving+=candidates[index];
+// solve(candidates,target,ans,index+1,current,solving);
+// current.pop_back();
+// solving-=candidates[index];
+// solve(candidates,target,ans,index+1,current,solving);
+        
+// }
+//     vector<vector<int>> combinationII(vector<int>& candidates, int target) {
+//         sort(candidates.begin(),candidates.end());
+//         set<vector<int>>ans;
+//         vector<vector<int>>ans1;
+//         int index=0;
+//         vector<int>current;
+//         int solving=0;
+//         solve(candidates,target,ans,index,current,solving);
+//         for(auto it:ans){
+//            ans1.push_back(it);
+//         }
+//         return ans1;
+//     }
+
+// optimal solution tc=2^n*k(where k is total uniqu combination)
+// void solve(vector<int>arr,int target,vector<vector<int>>&ans,vector<int>&current,int index){
+    
+//         if(target==0){
+//             ans.push_back(current);
+//             return;
+//         }
+        
+    
+//     for(int i=index;i<arr.size();i++){
+//         if(i>index&&arr[i]==arr[i-1]){
+//             continue;
+//         }
+//         if(arr[i]>target){
+//             break;
+//         }
+//         current.push_back(arr[i]);
+//         solve(arr,target-arr[i],ans,current,i+1);
+//         current.pop_back();
+//     }
+// }
+// void combinationII(vector<int>arr,int target){
+//     vector<vector<int>>ans;
+//     sort(arr.begin(),arr.end());
+//     vector<int>current;
+//     int index=0;
+//     solve(arr,target,ans,current,index);
+//     for(auto it:ans){
+//         for(auto x:it){
+//             cout<<x<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// void solve(vector<int>arr,vector<int>&ans,int sum,int index){
+//     if(index==arr.size()){
+//         ans.push_back(sum);
+//         return;
+//     }
+//     solve(arr,ans,sum+arr[index],index+1);
+//     solve(arr,ans,sum,index+1);
+// }
+// void sum_of_all_subset(vector<int>arr){
+    // brute force  O(2^n)
+    // int n=arr.size();
+    // vector<int>ans;
+    // for(int i=0;i<(1<<n);i++){
+    //     int sum=0;
+    //  for(int index=0;index<n;index++){
+    //     if(i&(1<<index)){
+    //         sum+=arr[index]; 
+    //     }
+    //  }
+    //  ans.push_back(sum);
+    // }
+    // for(auto it:ans){
+    //     cout<<it<<" ";
+    // }
+    // optimal solution O((2^n)*n)
+    // vector<int>ans;
+    // int index=0;
+    // int sum=0;
+    // solve(arr,ans,sum,index);
+    //   sort(ans.begin(),ans.end());
+    // for(auto it:ans){
+    //     cout<<it<<" ";
+    // }
+// }
+void solving(vector<int>nums,vector<vector<int>>&ans,int index,vector<int>solve){
+    ans.push_back(solve);
+    
+ for(int i=index;i<nums.size();i++){
+        if(i>index&&nums[i]==nums[i-1]){
+            continue;
+        }
+        solve.push_back(nums[i]);
+        solving(nums,ans,i+1,solve);
+        solve.pop_back();
     }
-    return ;
-  }
-  if(arr[index]<=target){
-    current.push_back(arr[index]);
-    solve(arr,target-arr[index],current,ans,index);
-    current.pop_back();
-  }
-  solve(arr,target,current,ans,index+1);
 }
-void combination_sum(vector<int>arr,int target){
-vector<int>current;
-vector<vector<int>>ans;
-int index=0;
-solve(arr,target,current,ans,index);
-for(auto it :ans){
-    for(auto x:it){
-        cout<<x<<" ";
-    }
-    cout<<endl;
-}
+void sum_of_all_subsetII(vector<int>nums){
+// brute (list use kar lo);
+    // optimal
+    sort(nums.begin(),nums.end());
+    vector<vector<int>>ans;
+    int index=0;
+    vector<int>solve;
+    solving(nums,ans,index,solve);
+    
+    
+
 }
 int main(){
 // int n=2;
@@ -176,12 +304,16 @@ int main(){
 // generate_all_binary_string(n);
 // generate_all_parenethesis(n);
 // generate_all_subsequence_of_string("abc");
-// vector<int>arr={1,2,1};
-// int sum=2;
+// vector<int>arr={1,2};
+// int sum=12;
 // print_all_subsequence_of_sum_2(sum,arr);
 // print_one_subsequence_of_sum_2(sum,arr);
 // cout<<no_of_subsequence_of_sum_2(sum,arr);
-vector<int>arr={2,3,6,7};
-combination_sum(arr,7);
+// vector<int>arr={10,1,2,7,6,1,5};
+// combination_sum(arr,7);
+// combinationII(arr,8);
+vector<int>arr={1,2,3};
+// sum_of_all_subset(arr);
+sum_of_all_subsetII(arr);
 
 }
