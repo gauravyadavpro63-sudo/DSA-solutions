446679988acfc01af16cf5594f029c836f2e9bd9
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-07-11 22:07:39 +0530

    recursion-hard-nqueen

--- step47.cpp
+++ step47.cpp
@@ -26,40 +26,132 @@ using namespace std;
 //     int cuts=solve(s,index)-1;
 //     return cuts;
 // }
-bool possibility(vector<vector<char>>&board,string &word,int rows,int columns,int index){
-    if(index==word.size()){
-        return true;
-    }
-    if(rows<0||columns<0||rows>=board.size()||columns>=board[0].size()||(word[index]!=board[rows][columns])){
-        return false;
-    }
-    char temp=board[rows][columns];
-    board[rows][columns]='#';
+// bool possibility(vector<vector<char>>&board,string &word,int rows,int columns,int index){
+//     if(index==word.size()){
+//         return true;
+//     }
+//     if(rows<0||columns<0||rows>=board.size()||columns>=board[0].size()||(word[index]!=board[rows][columns])){
+//         return false;
+//     }
+//     char temp=board[rows][columns];
+//     board[rows][columns]='#';
     
-    bool found=possibility(board,word,rows+1,columns,index+1)||possibility(board,word,rows-1,columns,index+1)||possibility(board,word,rows,columns-1,index+1)||possibility(board,word,rows,columns+1,index+1);
+//     bool found=possibility(board,word,rows+1,columns,index+1)||possibility(board,word,rows-1,columns,index+1)||possibility(board,word,rows,columns-1,index+1)||possibility(board,word,rows,columns+1,index+1);
     
-    board[rows][columns]=temp;
-    return found;
+//     board[rows][columns]=temp;
+//     return found;
 
 
 
-}
-bool word_search(vector<vector<char>>&board,string &word){
-   int index=0;
-   int rows=board.size();
-   int columns=board[0].size();
-   for(int i=0;i<rows;i++){
-    for(int j=0;j<columns;j++){
-        if(possibility(board,word,i,j,index)){
-            return true;
+// }
+// bool word_search(vector<vector<char>>&board,string &word){
+//    int index=0;
+//    int rows=board.size();
+//    int columns=board[0].size();
+//    for(int i=0;i<rows;i++){
+//     for(int j=0;j<columns;j++){
+//         if(possibility(board,word,i,j,index)){
+//             return true;
+//         }
+//     }
+//    }
+//    return false;
+// }
+// bool save(vector<string>&board,int column,int row,int n){
+// int drow=row;
+// int dcolumn=column;
+// while(row>=0&&column>=0){
+//      if(board[row][column]=='Q') return false;
+//     row--;
+//     column--;
+// }
+// row=drow;
+// column=dcolumn;
+// while(column>=0){
+//     if(board[row][column]=='Q')  return false;
+//     column--;
+// }
+// column=dcolumn;
+// while(column>=0&&row<n){
+//     if(board[row][column]=='Q') return false;
+//     row++;
+//     column--;
+// }
+// return true;
+
+
+// }
+// void solve(vector<string>&board,vector<vector<string>>&ans,int column,int n){
+// if(column==n){
+//     ans.push_back(board);
+//     return;
+// }
+// for(int row=0;row<n;row++){
+//  if(save(board,column,row,n)==true){
+//     board[row][column]='Q';
+//     solve(board,ans,column+1,n);
+//     board[row][column]='.';
+//  }
+// }
+// }
+
+
+
+void solve(int n,vector<string>&board,vector<vector<string>>&ans,int column,vector<int>&hashrow,vector<int>&lowerdiagonal,vector<int>&upperdiagonal){
+    if(column==n){
+        ans.push_back(board);
+        return ;;
+    }
+    for(int row=0;row<n;row++){
+        if(hashrow[row]==0&&lowerdiagonal[row+column]==0&&upperdiagonal[n-1+(row-column)]==0){
+            board[row][column]='Q';
+            hashrow[row]=1;
+            lowerdiagonal[row+column]=1;
+            upperdiagonal[n-1+(row-column)]=1;
+            solve(n,board,ans,column+1,hashrow,lowerdiagonal,upperdiagonal);
+            board[row][column]='.';
+            hashrow[row]=0;
+            lowerdiagonal[row+column]=0;
+            upperdiagonal[n-1+(row-column)]=0;
+            
         }
     }
-   }
-   return false;
+}
+vector<vector<string>> n_queens(int n){
+    // brute tc O(n!×n)
+    // vector<string>board;
+    // string s(n,'.');
+    // vector<vector<string>>ans;
+    // int column=0;
+    // for(int i=0;i<n;i++){
+    //     board.push_back(s);
+    // }
+    // solve(board,ans,column, n);
+    // return ans;
+    // better tc n!
+    vector<string>board;
+    string s(n,'.');
+    for(int i=0;i<n;i++){
+        board.push_back(s);
+    }
+    vector<vector<string>>ans;
+    int column=0;
+    vector<int>hashrow(n,0);
+    vector<int>lowerdiagonal(2*n-1,0);
+    vector<int>upperdiagonal(2*n-1,0);
+    solve(n,board,ans,column,hashrow,lowerdiagonal,upperdiagonal);
+    return ans;
+
 }
 int main(){
     // cout<<palindrom_partioning("aab");
-    vector<vector<char>>board={{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'},{'A', 'D', 'E', 'E'}};
-    string word="ABCCED";
-    cout<<word_search(board,word);
+    // vector<vector<char>>board={{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'},{'A', 'D', 'E', 'E'}};
+    // string word="ABCCED";
+    // cout<<word_search(board,word);
+    vector<vector<string>>ans =n_queens(4);
+    for(auto it:ans){
+    for(auto k:it){
+        cout<<k<<" ";
+    }
+}
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,50 @@
-        
+bool save(vector<string>&board,int column,int row,int n){
+int drow=row;
+int dcolumn=column;
+while(row>=0&&column>=0){
+     if(board[row][column]=='Q') return false;
+    row--;
+    column--;
+}
+row=drow;
+column=dcolumn;
+while(column>=0){
+    if(board[row][column]=='Q')  return false;
+    column--;
+}
+column=dcolumn;
+while(column>=0&&row<n){
+    if(board[row][column]=='Q') return false;
+    row++;
+    column--;
+}
+return true;
+
+
+}
+void solve(vector<string>&board,vector<vector<string>>&ans,int column,int n){
+if(column==n){
+    ans.push_back(board);
+    return;
+}
+for(int row=0;row<n;row++){
+ if(save(board,column,row,n)==true){
+    board[row][column]='Q';
+    solve(board,ans,column+1,n);
+    board[row][column]='.';
+ }
+}
+
+
+}
+vector<vector<string>> n_queens(int n){
+    vector<string>board;
+    string s(n,'.');
+    vector<vector<string>>ans;
+    int column=0;
+    for(int i=0;i<n;i++){
+        board.push_back(s);
+    }
+    solve(board,ans,column, n);
+    return ans;
+}
