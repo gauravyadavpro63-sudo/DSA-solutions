a58a316a6b2705fc63c74ebb43ecdf10e855de84
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-09-07 23:30:49 +0530

    minimum-platform

--- step63.cpp
+++ step63.cpp
@@ -21,17 +21,95 @@ using namespace std;
 //     }
 //     return ans;
 // }
-bool jump_game1(vector<int> arr){
-    // TC = O(N)
-// SC = O(1)
+// bool jump_game1(vector<int> arr){
+//     // TC = O(N)
+// // SC = O(1)
 
-    int n=arr.size();
-    int maxindex=0;
-    for(int i=0;i<n;i++){
-      if(i>maxindex) return false;
-      maxindex=max(maxindex,i+arr[i]);
+//     int n=arr.size();
+//     int maxindex=0;
+//     for(int i=0;i<n;i++){
+//       if(i>maxindex) return false;
+//       maxindex=max(maxindex,i+arr[i]);
+//     }
+//     return true;
+// }
+
+// int solve(vector<int>nums,int i){
+//     if(i==nums.size()-1){
+//         return 0;
+//     }
+//     int minimum=INT_MAX;
+//     for(int j=1;j<=nums[i];j++){
+//        if(i+j<nums.size()){
+//         minimum=min(minimum,1+solve(nums,i+j));
+//        }
+//     }
+//     return minimum;
+// }
+// int jump_game_2(vector<int>nums){
+    // brute solution
+    // TC = O(2ⁿ)
+    // SC = O(n)
+    // return solve(nums,0);
+    // optimal solution
+    // TC = O(n)
+    // int n=nums.size();
+    // int maxindex= 0;
+    // int currentindex=0;
+    // int jump=0;
+    // for(int i=0;i<n-1;i++){
+    //     maxindex=max(maxindex,i+nums[i]);
+    
+    // if(i==currentindex){
+    //     jump++;
+    //     currentindex=maxindex;
+    // }
+// }
+// return jump;
+
+// }
+
+int minimum_number_of_platform(vector<int>arrival,vector<int>departure){
+    // brute force tc = n*n
+    // int n=arrival.size();
+    // int ans=0;
+    // for(int i=0;i<n;i++){
+    //     int count=0;
+    //     int start=arrival[i];
+    //     for(int j=0;j<n;j++){
+    //         if(start>=arrival[j]&&start<=departure[j]){
+    //             count++;
+    //         }
+            
+    //     }
+    //      ans=max(ans,count);
+    // }
+    // return ans;
+ 
+ 
+    // optimal solution    tc=O(n);
+
+    int n=arrival.size();
+    sort(arrival.begin(),arrival.end());
+    sort(departure.begin(),departure.end());
+    int i=0;
+    int j=0;
+    int count=0;
+    int  platform=0;
+    while(i<n&&j<n){
+        if(arrival[i]<departure[j]){
+            i++;
+            platform++;
+            count=max(count,platform);
+        }
+        else{
+            j++;
+            platform--;
+        }
     }
-    return true;
+    return count;
+
+
 }
 int main(){
 // int  n = 6;
@@ -42,8 +120,14 @@ int main(){
 // for(auto it:ans){
 //     cout<<it<<" ";
 // }
- vector<int>arr= {2, 3, 1, 0, 4};
-cout<<jump_game1(arr);
+//  vector<int>arr= {2, 3, 1, 0, 4};
+// cout<<jump_game1(arr);
 
+// vector<int>nums = {2,3,1,1,4};
+// cout<<jump_game_2(nums);
 
+vector<int>arrival   = {900, 940, 950, 1100, 1500, 1800};
+vector<int>departure = {910, 1200, 1120, 1130, 1900, 2000};
+
+cout<<minimum_number_of_platform(arrival,departure);
 }
